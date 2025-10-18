aed5d4b406ca0806ec014a3ac46d16748f9744ba
Max <mrakul84@gmail.com>
2025-10-18 15:29:05 +0300

    s

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Header from "@/components/Header/Header";
 import Footer from "@/components/Footer/Footer";
-import TanstackProvider from "@/components/TanStackProvider/TanStackProvider";
+import TanStackProvider from "@/components/TanStackProvider/TanStackProvider";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -28,11 +28,11 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={`${geistSans.variable} ${geistMono.variable}`}>
-        <TanstackProvider>
+        <TanStackProvider>
           <Header />
           <main>{children}</main>
           <Footer />
-        </TanstackProvider>
+        </TanStackProvider>
       </body>
     </html>
   );
